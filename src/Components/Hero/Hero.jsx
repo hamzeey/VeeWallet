9f355a0bfb1275011d25bf2a7f9227749122e33c
Mac <habibullahamza@gmail.com>
2025-05-20 18:03:48 +0100

    hero section almost done

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,13 +1,29 @@
 import React from 'react'
 import './Hero.css'
+import setup from '../../assets/setup.png'
 function Hero() {
   return (
     <div className='hero'>
        <div className='hero-txt'>
+<h2>Buy, Trade and <span>Hodl</span> <br />your Favourite <br />Cryptocurrencies</h2>
+<p className='trade'>Trade Over 1200 Cryptocurrencies Safely, Quickly and Easily</p>
+<div className='inner-form'> 
+<form action="">
+  <div className='field'>
+  <input type="text" placeholder='Email Address' className='p-text'/>
+  </div>
 
+</form>
+<div>
+  <div><button className='btn'>Get Started</button></div>
+</div>
+</div>
+<p className='fees'>Sign Up Now and Get 0 Fees on Trading</p>
        </div>
-       <div className='hero-img'>
-
+       <div className='img-cont'>
+        <div className='hero-img'>
+<img src={setup} className='comp' />
+</div>
        </div>
     </div>
   )
--- src/Components/Numbers/Numbers.jsx
+++ src/Components/Numbers/Numbers.jsx
@@ -2,8 +2,25 @@ import React from 'react'
 import './Numbers.css'
 function Numbers() {
   return (
-    <div>
-        <h2 className='numbers'>Numbers</h2>
+    <div className='numbers'>
+       <div className='contaiiner'>
+<div>
+  <h1>$2.05 billion</h1>
+  <p>24h Trading volume</p>
+</div>
+<div>
+   <h1>1200+</h1>
+  <p>Cryptocurrencies Listed</p>
+</div>
+<div>
+   <h1>5+ million</h1>
+  <p>Registered Users</p>
+</div>
+<div>
+   <h1>0%</h1>
+  <p>lowest Trading Fees</p>
+</div>
+       </div>
     </div>
   )
 }
