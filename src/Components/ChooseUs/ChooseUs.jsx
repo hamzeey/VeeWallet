3257e704cddb046da98ba894a422439183d53e57
Mac <habibullahamza@gmail.com>
2025-05-22 16:24:20 +0100

    more sections completed

--- src/Components/ChooseUs/ChooseUs.jsx
+++ src/Components/ChooseUs/ChooseUs.jsx
@@ -1,11 +1,55 @@
-import React from 'react'
-import './ChooseUs.css'
+import React from "react";
+import "./ChooseUs.css";
+import img1 from "../../assets/reliable1.png";
+import img2 from "../../assets/reliable2.png";
+import img3 from "../../assets/reliable3.png";
+
+const data = [
+  {
+    id: 1,
+    image: img1,
+    title: "Secure Storage",
+    desc: "We ensure your funds are protected with both centralized and decentralized methods.",
+  },
+  {
+    id: 2,
+    image: img2,
+    title: "Insurance Protection",
+    desc: "All cryto cash and balances are maintained and insured by NIDC up to a maximum of $400,000.",
+  },
+  {
+    id: 3,
+    image: img3,
+    title: "Personalized Access Control",
+    desc: "Personalized access control allows you to restrict devices and addresses that can access your account, for greater ease of mind",
+  },
+];
 function ChooseUs() {
   return (
-    <div>
-        <h2 className='choose'>ChooseUs</h2>
+    <div className="choose">
+      <div className="header">
+        <h1>Your Most Reliable Crypto Exchange</h1>
+        <p>Here are some reasons why you should choose VeeWallet</p>
+      </div>
+
+      <div className="body">
+        {data.map(({ id, image, title, desc }) => {
+          return (
+            <div className="contaiiiiner" key={id}>
+              <div className="image-contaiiner">
+                <img src={image} alt="" />
+              </div>
+
+              <div className="teext">
+                <h4>{title}</h4>
+                <p>{desc}</p>
+              </div>
+            </div>
+          );
+        })}
+      </div>
     </div>
-  )
+  );
 }
 
-export default ChooseUs
+export default ChooseUs;
--- src/Components/Staking/Staking.jsx
+++ src/Components/Staking/Staking.jsx
@@ -1,11 +1,135 @@
-import React from 'react'
-import './Staking.css'
+import React from "react";
+import "./Staking.css";
+import sol from "../../assets/L-sol.png";
+import bnb from "../../assets/L-bnb.png";
+import atom from "../../assets/L-atom.png";
+import wan from "../../assets/L-wan.png";
+import dash from "../../assets/L-dash.png";
+import digi from "../../assets/L-digi.png";
+import ont from "../../assets/L-ont.png";
+import via from "../../assets/L-via.png";
+import usdt from "../../assets/L-tether.png";
+import neo from "../../assets/L-neo.png";
+
+const data = [
+  {
+    id: 1,
+    image: sol,
+    title: "Solana ",
+    short:'(SOL)',
+    desc: "Annual rewards",
+    price: "9-12%",
+
+  },
+  {
+    id: 2,
+    image: bnb,
+    title: "Binance",
+     short:'(BNB)',
+    desc: "Annual rewards",
+    price: "12-30%",
+  },
+  {
+    id: 3,
+    image: atom,
+    title: "Cosmos",
+     short:'(ATOM)',
+    desc: "Annual rewards",
+    price: "8-14%",
+  },
+  {
+    id: 4,
+    image: wan,
+    title: "Wanchain",
+     short:'(WAN)',
+    desc: "Annual rewards",
+    price: "6-10%",
+  },
+  {
+    id: 5,
+    image: dash,
+    title: "Dash",
+     short:'(DASH)',
+    desc: "Annual rewards",
+    price: "5-12%",
+  },
+  {
+    id: 6,
+    image: digi,
+    title: "DigiByte ",
+     short:'(DGB)',
+    desc: "Annual rewards",
+    price: "15-25%",
+  },
+  {
+    id: 7,
+    image: ont,
+    title: "Ontology",
+     short:'(ONT)',
+    desc: "Annual rewards",
+    price: "20-30%",
+  },
+  {
+    id: 8,
+    image: via,
+    title: "Viacoin",
+     short:'(VIA)',
+    desc: "Annual rewards",
+    price: "9-14%",
+  },
+  {
+    id: 9,
+    image: usdt,
+    title: "Tether",
+     short:'(USDT)',
+    desc: "Annual rewards",
+    price: "10-20%",
+  },
+  {
+    id: 10,
+    image: neo,
+    title: "Neo ",
+     short:'(NEO)',
+    desc: "Annual rewards",
+    price: "8-12%",
+  },
+];
+
 function Staking() {
   return (
-    <div>
-        <h2 className='staking'>Staking</h2>
+    <div className="staking">
+      <div className="stake-txt">
+        <h5>STAKING CRYPTO</h5>
+        <h2>Earn Crypto Rewards</h2>
+        <p>Earn up to 40% in reward yearly by staking your assets</p>
+      </div>
+
+      <div className="main-wrapper">
+        <div className="card-wrapper">
+          {data.map(({ id, image, title, desc, price,short }) => {
+            return (
+              <div className="contaiiiner" key={id}>
+                <div className="img-container">
+                  <img src={image} alt="" />
+                  <div className="img-hover"></div>
+                </div>
+                <div className="text">
+                  <h4>{title} <span>{short}</span> </h4>
+                  <h5>{desc}</h5>
+                  <p className="price">{price}</p>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+
+      </div>
+      <div className="button">
+      <button className="btn"> Start Earning &gt;</button>
+      </div>
+
     </div>
-  )
+  );
 }
 
-export default Staking
+export default Staking;
